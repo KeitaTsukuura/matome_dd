d54734217e8df8024fac108062cc778f6ff180af
Keita Tsukuura <tsukuura.keita@gmail.com>
2024-05-16 17:46:51 +0900

    投稿編集機能の追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -19,10 +19,20 @@ class PostController extends Controller
     {
         return view('posts.create');
     }
+    public function edit(Post $post)
+    {
+        return view('posts.edit')->with(['post' => $post]);
+    }
     public function store(PostRequest $request, Post $post)
     {
         $input = $request['post'];
         $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    public function update(PostRequest $request, Post $post)
+    {
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
         return redirect('/posts/' . $post->id);
     }
 }
